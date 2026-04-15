4b8eae8ef62c78c0548d94f0efab0647a722c224
Marcos Oliveira <marcoslucas.dev@gmail.com>
2026-04-15 13:36:01 -0300

    fix: resolve clienteUid pelo CPF ao salvar aulas agendadas no modal de contratação

--- functions-banco-de-aulas-Cards.js
+++ functions-banco-de-aulas-Cards.js
@@ -1,1 +1,11 @@
-PLACEHOLDER
+console.log('✅ functions-banco-de-aulas-Cards.js carregado');
+
+// Objeto global para expor as funções
+const BancoDeAulasCards = (function() {
+  // Variáveis privadas
+  let aulasData = [];
+  let currentFilters = {};
+  
+  // Função para renderizar cards de aulas
+  function renderAulasCards(aulas, filters = {}) {
+    console.log('🎴 Renderizando cards:', aulas.length);
